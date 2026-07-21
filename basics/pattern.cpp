1180e5e45128cc018d213114fa01df2e2bc8ed02
bhavesh <bhaveshdangi112006@gmail.com>
2026-07-21 19:36:06 +0530

    added more pattern

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -156,17 +156,66 @@ int main()
 // }
 
 // 13. inverted triangle pattern
+// int n=4;
+// for (int i = 0; i <n; i++)
+// {
+//     for( int j=0;j<i;j++){
+//         cout<<" ";
+//     }
+//      for( int j=0;j<n-i;j++){
+//         cout<<(i+1);
+        
+//     }
+//      cout<<endl;
+// }
+
+//14. inverted alphabets triangle pattern
+//  int n=4;
+//   char ch=65;
+// for (int i = 0; i <n; i++)
+// {
+//     for( int j=0;j<i;j++){
+//         cout<<" ";
+//     }
+//      for( int j=0;j<n-i;j++){
+//         cout<<ch;
+        
+//     }
+//     ch++;
+//      cout<<endl;
+// }
+
+//15. pyramid pattern
+// int n=4;
+// for(int i=1;i<=n;i++){
+//     for(int j=n-1;j>=i;j--){
+//         cout<<" ";
+//     }
+//     for(int j=1;j<=i;j++){
+//         cout<<j;
+//     }
+
+//     for(int j=i-1;j>=1;j--){
+//         cout<<j;
+//     }
+//     cout<<endl;
+// }
+
+//16. hollow pyramid pattern
 int n=4;
-for (int i = 0; i <n; i++)
-{
-    for( int j=0;j<i;j++){
+for(int i=1;i<=n;i++){
+    for(int j=n-1;j>=i;j--){
         cout<<" ";
     }
-     for( int j=0;j<n-i;j++){
-        cout<<(i+1);
-        
-    }
-     cout<<endl;
-}
+    cout<<"*";
+    for(int j=1;j<=2*(i-1);j++){
+            cout<<" ";
 
-}
+    }
+        if(i!=1){
+      cout<<"*";
+        }
+ 
+    cout<<endl;
+
+}}
