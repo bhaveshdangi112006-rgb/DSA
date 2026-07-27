be79d69a18b715d51f1c53bef5ff4385a5ab4caf
bhavesh <bhaveshdangi112006@gmail.com>
2026-07-27 21:00:52 +0530

    learn basic math login probelms

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -202,21 +202,29 @@ int main()
 // }
 
 //16. hollow pyramid pattern
+// int n=4;
+// for(int i=1;i<=n;i++){
+//     for(int j=n-1;j>=i;j--){
+//         cout<<" ";
+//     }
+//     cout<<"*";
+//     for(int j=1;j<=2*(i-1);j++){
+//             cout<<" ";
+
+//     }
+//         if(i!=1){
+//       cout<<"*";
+//         }
+ 
+//     cout<<endl;
+
+// }
 int n=4;
 for(int i=1;i<=n;i++){
-    for(int j=n-1;j>=i;j--){
-        cout<<" ";
+    for(int j=1;j<=n;j++){
+        cout<<"*";
     }
-    cout<<"*";
-    for(int j=1;j<=2*(i-1);j++){
-            cout<<" ";
-
-    }
-        if(i!=1){
-      cout<<"*";
-        }
- 
     cout<<endl;
-
 }
+return 0;
 }
